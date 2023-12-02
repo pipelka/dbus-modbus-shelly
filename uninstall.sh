6c25cfa9a690eb0d93f00fbbe35de900eca7730f
Alexander Pipelka <alexander.pipelka@gmail.com>
2023-12-02 19:27:23 +0100

    uninstall: updated script

--- uninstall.sh
+++ uninstall.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
-SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}"  )" &> /dev/null && pwd  )
-DAEMON_NAME=${SCRIPT_DIR##*/}
-
-rm /service/$DAEMON_NAME
-kill $(pgrep -f "python $SCRIPT_DIR/$DAEMON_NAME")
-kill $(pgrep -f 'supervise $DAEMON_NAME')
+SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
+SERVICE_NAME=$(basename $SCRIPT_DIR)
+filename=/data/rc.local
+rm /service/$SERVICE_NAME
+kill $(pgrep -f 'supervise dbus-modbus-shelly')
 chmod a-x $SCRIPT_DIR/service/run
 $SCRIPT_DIR/restart.sh
+STARTUP=$SCRIPT_DIR/install.sh
+sed -i "\~$STARTUP~d" $filename
